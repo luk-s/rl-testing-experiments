cfccc916c551c17668edbab9df990bdded6d7696
Lukas Fluri <lukas.fluri@protonmail.ch>
2023-01-04 07:35:34 +0100

    Changed default experiment parameters.

--- experiments/score_positions.py
+++ experiments/score_positions.py
@@ -203,9 +203,10 @@ if __name__ == "__main__":
     parser.add_argument("--engine_config_name",     type=str, default="local_400_nodes.ini")  # noqa: E501
     # parser.add_argument("--engine_config_name",     type=str, default="remote_400_nodes.ini")  # noqa: E501
     parser.add_argument("--data_config_name",       type=str, default="interesting_fen_database.ini")  # noqa: E501
-    parser.add_argument("--network_name",           type=str, default="T807301-c85375d37b369db8db6b0665d12647e7a7a3c9453f5ba46235966bc2ed433638")  # noqa: E501
+    # parser.add_argument("--network_name",           type=str, default="T807301-c85375d37b369db8db6b0665d12647e7a7a3c9453f5ba46235966bc2ed433638")  # noqa: E501
+    parser.add_argument("--network_name",           type=str, default="T785469-600469c425eaf7397138f5f9edc18f26dfaf9791f365f71ebc52a419ed24e9f2")  # noqa: E501    
     parser.add_argument("--num_positions",          type=int, default=216)
-    parser.add_argument("--num_evals_per_position", type=int, default=20)
+    parser.add_argument("--num_evals_per_position", type=int, default=100)
     parser.add_argument("--result_subdir",          type=str, default="")
     # fmt: on
     ##################################
