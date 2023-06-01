a691b0444fb57c46f5a4f9181710f77b9a10e440
Lukas Fluri <lukas.fluri@protonmail.ch>
2023-06-01 21:41:33 +0200

    Clean plotting code.

--- experiments/analysis/plot_distributions.sh
+++ experiments/analysis/plot_distributions.sh
@@ -7,30 +7,23 @@
 # Select which plots should be created
 CREATE_FORCED_MOVES="True"
 CREATE_BEST_MOVES_MIDDLEGAMES="True"
-CREATE_BEST_MOVES_ENDGAMES="False"
-CREATE_MIRROR_BOARD="False"
-CREATE_TRANSFORMATION_BOARD="False"
+CREATE_MIRROR_BOARD="True"
+CREATE_TRANSFORMATION_BOARD="True"
 
 # Some general options
-SAVE_RESULT_CSV="True"
+SAVE_RESULT_CSV="False"
 SAVE_PLOT="True"
 SHOW_PLOT="False"
 
 # Define the names of the result files
-# FILE_FORCED_MOVES="experiments/results/final_data/parent_child_results_ENGINE_local_400_nodes_DATA_final_forced_moves_master_fen.txt"
-FILE_FORCED_MOVES="temp/results_ENGINE_local_400_nodes_DATA_all_forced_move_positions_fen_2023_05_07_21:34:15.txt"
-# FILE_BEST_MOVES_MIDDLEGAMES="experiments/results/final_data/parent_child_results_ENGINE_local_400_nodes_DATA_final_middlegame_master_dense_fen.txt"
-# FILE_BEST_MOVES_MIDDLEGAMES="experiments/results/parent_child_testing/results_ENGINE_local_400_nodes_DATA_parent_child_experiment_middlegames_most_important_fens_2023_05_07_17:11:38.txt"
-FILE_BEST_MOVES_MIDDLEGAMES="temp/results_ENGINE_local_400_nodes_DATA_all_middlegame_positions_fen_2023_05_07_21:32:30.txt"
-# FILE_BEST_MOVES_MIDDLEGAMES="temp/results_ENGINE_local_400_nodes_2draw_DATA_parent_child_experiment_middlegames_most_important_fens_2023_05_08_08:30:34.txt"
-FILE_BEST_MOVES_ENDGAMES="experiments/results/final_data/parent_child_results_ENGINE_local_400_nodes_DATA_final_endgame_master_dense_fen.txt"
-FILE_MIRROR_BOARD="experiments/results/final_data/mirror_results_ENGINE_local_400_nodes_DATA_final_middlegame_master_dense_fen.txt"
-FILE_TRANSFORMATION_BOARD="experiments/results/final_data/transform_results_ENGINE_local_400_nodes_DATA_final_no_pawns_synthetic_fen_combined.txt"
+FILE_FORCED_MOVES="experiments/results/final_data/forced_move_400_nodes_400k_forced_move_positions.csv"
+FILE_BEST_MOVES_MIDDLEGAMES="experiments/results/final_data/recommended_move_400_nodes_400k_middlegame_positions.csv"
+FILE_MIRROR_BOARD="experiments/results/final_data/mirror_position_400_nodes_400k_middlegame_positions.csv"
+FILE_TRANSFORMATION_BOARD="experiments/results/final_data/board_transformation_400_nodes_200k_no_pawns_synthetic_positions.csv"
 
 # Define the titles of the plots
-TITLE_FORCED_MOVES="Forced moves [Experiment in progress]"
-TITLE_BEST_MOVES_MIDDLEGAMES="Best moves (middlegames) [Experiment in progress]"
-TITLE_BEST_MOVES_ENDGAMES="Best moves (endgames)"
+TITLE_FORCED_MOVES="Forced moves"
+TITLE_BEST_MOVES_MIDDLEGAMES="Recommended moves (middlegames)"
 TITLE_MIRROR_BOARD="Mirror board"
 TITLE_TRANSFORMATION_BOARD="Transform board"
 
@@ -39,7 +32,8 @@ TITLE_TRANSFORMATION_BOARD="Transform board"
 COMMON_ARGS="--x_limit_min 0 --x_limit_max 2"
 
 # Arguments for parent-child-type experiments
-PARENT_CHILD_EXPERIMENT_ARGS="--column_name1 parent_score --column_name2 child_score --q_vals_to_flip child_score"
+# Optionally add this parameter --q_vals_to_flip child_score if you're using data directly coming from experiments
+PARENT_CHILD_EXPERIMENT_ARGS="--column_name1 score1 --column_name2 score2"
 
 # Arguments for mirror-type experiments
 MIRROR_EXPERIMENT_ARGS="--column_name1 original --column_name2 mirror"
@@ -85,13 +79,6 @@ if [ $CREATE_BEST_MOVES_MIDDLEGAMES == "True" ]; then
     echo "Best moves (middlegame)"
     echo "======================="
     python3 experiments/analysis/plot_distribution.py --result_path $FILE_BEST_MOVES_MIDDLEGAMES --title $TITLE_BEST_MOVES_MIDDLEGAMES $COMMON_ARGS $PARENT_CHILD_EXPERIMENT_ARGS
-fi
-
-if [ $CREATE_BEST_MOVES_ENDGAMES == "True" ]; then
-    echo "====================="
-    echo "Best moves (endgames)"
-    echo "====================="
-    python3 experiments/analysis/plot_distribution.py --result_path $FILE_BEST_MOVES_ENDGAMES --title $TITLE_BEST_MOVES_ENDGAMES $COMMON_ARGS $PARENT_CHILD_EXPERIMENT_ARGS
 fi
 
 if [ $CREATE_MIRROR_BOARD == "True" ]; then
