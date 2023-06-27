08b96a524cd3995c87aa770ab92555c072733070
Lukas Fluri <lukas.fluri@protonmail.ch>
2023-06-27 15:32:19 +0200

    Adding plotting code for differential testing experiment.

--- experiments/analysis/plot_distributions.sh
+++ experiments/analysis/plot_distributions.sh
@@ -5,27 +5,30 @@
 ##################################
 
 # Select which plots should be created
-CREATE_FORCED_MOVES="True"
-CREATE_BEST_MOVES_MIDDLEGAMES="True"
-CREATE_MIRROR_BOARD="True"
-CREATE_TRANSFORMATION_BOARD="True"
+CREATE_FORCED_MOVES="False" #"True"
+CREATE_BEST_MOVES_MIDDLEGAMES="False" #"True"
+CREATE_MIRROR_BOARD="False" #"True"
+CREATE_TRANSFORMATION_BOARD="False" #"True"
+CREATE_DIFFERENTIAL_TESTING="True"
 
 # Some general options
-SAVE_RESULT_CSV="False"
-SAVE_PLOT="True"
-SHOW_PLOT="False"
+SAVE_RESULT_CSV="True" # "False"
+SAVE_PLOT="False" #"True"
+SHOW_PLOT="True" #"False"
 
 # Define the names of the result files
 FILE_FORCED_MOVES="experiments/results/final_data/forced_move_400_nodes_400k_forced_move_positions.csv"
 FILE_BEST_MOVES_MIDDLEGAMES="experiments/results/final_data/recommended_move_400_nodes_400k_middlegame_positions.csv"
 FILE_MIRROR_BOARD="experiments/results/final_data/mirror_position_400_nodes_400k_middlegame_positions.csv"
 FILE_TRANSFORMATION_BOARD="experiments/results/final_data/board_transformation_400_nodes_200k_no_pawns_synthetic_positions.csv"
+FILE_DIFFERENTIAL_TESTING="experiments/results/final_data2/differential_testing_leela_400_nodes_T80_vs_leela_285_nodes_T78.txt"
 
 # Define the titles of the plots
 TITLE_FORCED_MOVES="Forced moves"
 TITLE_BEST_MOVES_MIDDLEGAMES="Recommended moves"
 TITLE_MIRROR_BOARD="Mirror board"
 TITLE_TRANSFORMATION_BOARD="Transform board"
+TITLE_DIFFERENTIAL_TESTING="Differential testing"
 
 ##  Define the groups of experiments which use the same arguments
 # Arguments for all experiments
@@ -40,6 +43,9 @@ MIRROR_EXPERIMENT_ARGS="--column_name1 original --column_name2 mirror"
 
 # Arguments for transformation-type experiments
 TRANSFORMATION_EXPERIMENT_ARGS="--column_names original rot90 rot180 rot270 flip_diag flip_anti_diag flip_hor flip_vert"
+
+# Arguments for differential testing
+DIFFERENTIAL_TESTING_ARGS="--column_name1 score1 --column_name2 score2"
 
 ##################################
 #           CONFIG END           #
@@ -93,4 +99,11 @@ if [ $CREATE_TRANSFORMATION_BOARD == "True" ]; then
     echo "Transformation board"
     echo "====================="
     python3 experiments/analysis/plot_distribution.py --result_path $FILE_TRANSFORMATION_BOARD --title $TITLE_TRANSFORMATION_BOARD $COMMON_ARGS $TRANSFORMATION_EXPERIMENT_ARGS
+fi
+
+if [ $CREATE_DIFFERENTIAL_TESTING == "True" ]; then
+    echo "====================="
+    echo "Differential testing"
+    echo "====================="
+    python3 experiments/analysis/plot_distribution.py --result_path $FILE_DIFFERENTIAL_TESTING --title $TITLE_DIFFERENTIAL_TESTING $COMMON_ARGS $DIFFERENTIAL_TESTING_ARGS
 fi
