e671c6bf8c4bf03df46dae189569928936a971e3
Lukas Fluri <lukas.fluri@protonmail.ch>
2023-07-09 09:07:01 +0200

    Added scripts to facilitate extraction of percentages from result files.

---
+++ experiments/analysis/print_result_percentages.py
@@ -0,0 +1,115 @@
+import argparse
+import math as m
+from pathlib import Path
+from typing import List, Tuple
+
+import pandas as pd
+
+
+# DATA_PATH = Path("experiments/results/final_data/")
+def get_num_samples_and_score_columns(file_name: str) -> Tuple[int, List[str]]:
+    if "400k" in file_name:
+        num_samples = 400_000
+    elif "200k" in file_name:
+        num_samples = 200_000
+    elif "100k" in file_name:
+        num_samples = 100_000
+    elif "50k" in file_name:
+        num_samples = 50_000
+
+    if "evolutionary_algorithm" in file_name:
+        score_columns = ["fitness1", "fitness2"]
+    elif "50k" in file_name and "board_transformation" in file_name:
+        score_columns = ["original", "rot180"]
+    elif "board_transformation" in file_name:
+        score_columns = [
+            "original",
+            "rot90",
+            "rot180",
+            "rot270",
+            "flip_diag",
+            "flip_anti_diag",
+            "flip_hor",
+            "flip_vert",
+        ]
+    elif "mirror" in file_name:
+        score_columns = ["original", "mirror"]
+    elif "recommended_move" in file_name or "forced_move" in file_name:
+        score_columns = ["score1", "score2"]
+    elif "differential_testing" in file_name:
+        score_columns = ["score1", "score2"]
+    else:
+        raise ValueError(f"Unknown file name: {file_name}")
+
+    return num_samples, score_columns
+
+
+def get_percentages(raw_row: str) -> str:
+    numbers = raw_row.split(" & ")
+    original_divisor = numbers[0]
+
+    numbers[0] = numbers[0].replace("k", "000")
+    divisor = numbers[0]
+
+    numbers = [int(number) for number in numbers]
+
+    divisor = numbers[0]
+
+    def round_to_1(x):
+        if x == 0:
+            return 0
+        if x > 1:
+            return round(x, 1)
+        return round(x, -int(m.floor(m.log10(abs(x)))))
+
+    def to_string(x):
+        if x == 0:
+            return "0\%"
+        if x < 0.01:
+            return "<0.01\%"
+        return f"{x}\%"
+
+    numbers = [round_to_1(100 * (number / divisor)) for number in numbers]
+
+    numbers = [original_divisor] + [to_string(number) for number in numbers[1:]]
+    return " & ".join(numbers)
+
+
+def get_numbers(
+    file_name: str, score_columns: List[str], num_samples: int, bins: List[float]
+) -> str:
+    # Load csv file
+    df = pd.read_csv(file_name)
+
+    # Compute the maximum difference between any two scores in the score columns
+    max_diff = df[score_columns].max(axis=1) - df[score_columns].min(axis=1)
+
+    # For each bin, compute the number of differences which are larger than the value of the bin
+    num_diffs = []
+    for bin in bins:
+        num_diffs.append((max_diff > bin).sum())
+
+    return f"{num_samples // 1000}k & " + " & ".join([str(num_diff) for num_diff in num_diffs])
+
+
+if __name__ == "__main__":
+    # fmt: off
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--result_path", type=str, help="Path to result file", required=True)  # noqa
+    parser.add_argument("--mode", type=str, help="Mode", choices=["numbers", "percentages"], default="percentages")  # noqa
+    # fmt: on
+    args = parser.parse_args()
+
+    file_name = args.result_path
+
+    bins = [0.05, 0.1, 0.25, 0.5, 0.75, 1.0]
+
+    num_samples, score_columns = get_num_samples_and_score_columns(file_name)
+
+    number_row = get_numbers(file_name, score_columns, num_samples, bins)
+
+    if args.mode == "numbers":
+        print(number_row)
+    elif args.mode == "percentages":
+        percentage_row = get_percentages(number_row)
+        print(percentage_row)
---
+++ experiments/analysis/print_result_percentages.sh
@@ -0,0 +1,87 @@
+RESULT_DIR="/home/lukas/Polybox_ETH/Master-Thesis/Code/rl-testing-experiments/experiments/results/final_data"
+LEELA_DIR="leela_chess_zero_results"
+EVOLUTIONARY_ALGORITHM_DIR="evolutionary_algorithm_results"
+STOCKFISH_CLASSIC_DIR="stockfish_classic_results"
+STOCKFISH_NNUE_DIR="stockfish_nnue_results"
+
+PRINT_LEELA_RESULTS="True"
+PRINT_SCALING_RESULTS="True"
+PRINT_EVOLUTIONARY_ALGORITHM_RESULTS="True"
+PRINT_STOCKFISH_CLASSIC_RESULTS="True"
+PRINT_STOCKFISH_NNUE_RESULTS="True"
+
+if [ $PRINT_LEELA_RESULTS == "True" ]; then
+    echo "======================"
+    echo "Printing Leela results"
+    echo "======================"
+    echo "Board transformations"
+    python3 experiments/analysis/print_result_percentages.py --result_path $RESULT_DIR/$LEELA_DIR/board_transformation_400_nodes_200k_no_pawns_synthetic_positions.csv
+    echo "Recommended moves"
+    python3 experiments/analysis/print_result_percentages.py --result_path $RESULT_DIR/$LEELA_DIR/recommended_move_400_nodes_400k_middlegame_positions.csv
+    echo "Differential testing"
+    python3 experiments/analysis/print_result_percentages.py --result_path $RESULT_DIR/$LEELA_DIR/differential_testing_leela_400_nodes_T80_vs_leela_285_nodes_T78_400k_middlegame_positions.csv
+    echo "Forced moves"
+    python3 experiments/analysis/print_result_percentages.py --result_path $RESULT_DIR/$LEELA_DIR/forced_move_400_nodes_400k_forced_move_positions.csv
+    echo "Position mirroring"
+    python3 experiments/analysis/print_result_percentages.py --result_path $RESULT_DIR/$LEELA_DIR/mirror_position_400_nodes_400k_middlegame_positions.csv
+fi
+
+if [ $PRINT_SCALING_RESULTS == "True" ]; then
+    echo "========================"
+    echo "Printing scaling results"
+    echo "========================"
+    echo "Recommended moves, 1 node"
+    python3 experiments/analysis/print_result_percentages.py --result_path $RESULT_DIR/$LEELA_DIR/recommended_move_1_node_100k_middlegame_positions.csv
+    echo "Recommended moves, 100 nodes"
+    python3 experiments/analysis/print_result_percentages.py --result_path $RESULT_DIR/$LEELA_DIR/recommended_move_100_nodes_100k_middlegame_positions.csv
+    echo "Recommended moves, 200 nodes"
+    python3 experiments/analysis/print_result_percentages.py --result_path $RESULT_DIR/$LEELA_DIR/recommended_move_200_nodes_100k_middlegame_positions.csv
+    echo "Recommended moves, 400 nodes"
+    python3 experiments/analysis/print_result_percentages.py --result_path $RESULT_DIR/$LEELA_DIR/recommended_move_400_nodes_100k_middlegame_positions.csv
+    echo "Recommended moves, 800 nodes"
+    python3 experiments/analysis/print_result_percentages.py --result_path $RESULT_DIR/$LEELA_DIR/recommended_move_800_nodes_100k_middlegame_positions.csv
+    echo "Recommended moves, 1600 nodes"
+    python3 experiments/analysis/print_result_percentages.py --result_path $RESULT_DIR/$LEELA_DIR/recommended_move_1600_nodes_100k_middlegame_positions.csv
+fi
+
+if [ $PRINT_EVOLUTIONARY_ALGORITHM_RESULTS == "True" ]; then
+    echo "========================================"
+    echo "Printing evolutionary algorithm results"
+    echo "========================================"
+    echo "Random boards"
+    python3 experiments/analysis/print_result_percentages.py --result_path $RESULT_DIR/$LEELA_DIR/board_transformation_1600_nodes_50k_no_pawns_synthetic_positions.csv
+    echo "Adversarial boards, run 1"
+    python3 experiments/analysis/print_result_percentages.py --result_path $RESULT_DIR/$EVOLUTIONARY_ALGORITHM_DIR/board_transformation_1600_nodes_50k_no_pawns_synthetic_positions_evolutionary_algorithm1_filtered_sorted.csv
+    echo "Adversarial boards, run 2"
+    python3 experiments/analysis/print_result_percentages.py --result_path $RESULT_DIR/$EVOLUTIONARY_ALGORITHM_DIR/board_transformation_1600_nodes_50k_no_pawns_synthetic_positions_evolutionary_algorithm2_filtered_sorted.csv
+    echo "Adversarial boards, run 3"
+    python3 experiments/analysis/print_result_percentages.py --result_path $RESULT_DIR/$EVOLUTIONARY_ALGORITHM_DIR/board_transformation_1600_nodes_50k_no_pawns_synthetic_positions_evolutionary_algorithm3_filtered_sorted.csv
+fi
+
+if [ $PRINT_STOCKFISH_CLASSIC_RESULTS == "True" ]; then
+    echo "==================================="
+    echo "Printing Stockfish Classic results"
+    echo "==================================="
+    echo "Recommended moves"
+    python3 experiments/analysis/print_result_percentages.py --result_path $RESULT_DIR/$STOCKFISH_CLASSIC_DIR/stockfish_classic_recommended_move_1400000_nodes_200k_middlegame_positions_q_scores_differences_sorted.csv
+    echo "Position mirroring"
+    python3 experiments/analysis/print_result_percentages.py --result_path $RESULT_DIR/$STOCKFISH_CLASSIC_DIR/stockfish_classic_mirror_position_1400000_nodes_200k_middlegame_positions_q_scores_differences_sorted.csv
+    echo "Forced moves"
+    python3 experiments/analysis/print_result_percentages.py --result_path $RESULT_DIR/$STOCKFISH_CLASSIC_DIR/stockfish_classic_forced_move_1400000_nodes_200k_forced_move_positions_q_scores_differences_sorted.csv
+    echo "Board transformations"
+    python3 experiments/analysis/print_result_percentages.py --result_path $RESULT_DIR/$STOCKFISH_CLASSIC_DIR/stockfish_classic_board_transformation_1400000_nodes_200k_no_pawns_synthetic_positions_q_scores_differences_sorted.csv
+fi
+
+if [ $PRINT_STOCKFISH_NNUE_RESULTS == "True" ]; then
+    echo "================================"
+    echo "Printing Stockfish NNUE results"
+    echo "================================"
+    echo "Recommended moves"
+    python3 experiments/analysis/print_result_percentages.py --result_path $RESULT_DIR/$STOCKFISH_NNUE_DIR/stockfish_nnue_recommended_move_81000_nodes_400k_middlegame_positions_q_scores_differences_sorted.csv
+    echo "Position mirroring"
+    python3 experiments/analysis/print_result_percentages.py --result_path $RESULT_DIR/$STOCKFISH_NNUE_DIR/stockfish_nnue_mirror_position_81000_nodes_400k_middlegame_positions_q_scores_differences_sorted.csv
+    echo "Forced moves"
+    python3 experiments/analysis/print_result_percentages.py --result_path $RESULT_DIR/$STOCKFISH_NNUE_DIR/stockfish_nnue_forced_move_81000_nodes_400k_forced_move_positions_q_scores_differences_sorted.csv
+    echo "Board transformations"
+    python3 experiments/analysis/print_result_percentages.py --result_path $RESULT_DIR/$STOCKFISH_NNUE_DIR/stockfish_nnue_board_transformation_81000_nodes_200k_no_pawns_synthetic_positions_q_scores_differences_sorted.csv
+fi
